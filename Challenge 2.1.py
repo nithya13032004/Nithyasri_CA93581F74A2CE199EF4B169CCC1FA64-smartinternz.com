86fd790a53a41d1f2cc36df56a9d05359100e183
nithya13032004 <145001952+nithya13032004@users.noreply.github.com>
2023-10-24 08:21:23 +0530

    Update Challenge 2.1.py

--- Challenge 2.1.py
+++ Challenge 2.1.py
@@ -1,15 +1,3 @@
-class Player:
- def play(self):
-   print("The player is playing cricket")
-class Batsman(Player):
- def play(self):
-  print("The batsman is batting.")
-class Bowler(Player):
- def play(self):
-   print("The bowler is Bowling")
-batsman=Batsman()
-bowler=Bowler()
-batsman.play()
-bowler.play()
+
 
 
